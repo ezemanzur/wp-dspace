96956e02554dc3337eaf49e420ee513a975cd6f1
Ezequiel Manzur <ezemanzur04@gmail.com>
2017-04-03 20:05:10 -0300

    Se agrego la funcion para construir la url

--- util/QueryBuilder.php
+++ util/QueryBuilder.php
@@ -2,36 +2,64 @@
 
 /** 
    * Esta clase se encarga de construir la url de consulta, utilizando los parametros de configuración obtenidos de la clase RespositoryHandler (url base, nombre del repositorio, etc) y los demas paremetros necesarios seteados en la variable $query_vars (handle, autor, etc)
+   * @author ezemanzur
+   * @project wp-dspace
+   * @url http://sedici.unlp.edu.ar 
 **/
 class QueryBuilder{
 
-protected $query_vars
-
-public function __consturct (){
-	$this->init()
+protected $query_vars;
+protected $urlBase;
+protected $queryPath;
+public function __consturct ($urlBase,$queryPath=""){
+	$this->$urlBase=$this->checkUrlBase($urlBase);
+	$this->$queryPath=$this->checkQueryPath($queryPath);
+	$this->init();
 }
-/** 
-   * 'start' es un parametro que nos va a permitir obtener la totalidad de la consulta.
-   *Cada consulta trae como máximo 100 resultados de todos los posibles, paginando el total y siendo 'start' el numero de item por el cual se comienza la busqueda. ( ejemplo item 101, página 2).
-**/
+private function checkUrlBase($urlBase){
+	if (substr($urlBase, -1)!="/")
+		$urlBase.="/";
+	return $urlBase;
+}
+private function checkQueryPath($queryPath){
+	if (substr($queryPath, -1)!="?" && !empty($queryPath))
+		$queryPath.="?";
+	return $queryPath;
+}
 public function init(){
 	$query_vars= array();
-	$this->add_query_vars('start',0);
 }
 /** 
    * metodo encargado de construir la url
 **/
-public function build($handlerRepository){
-
+public function build($urlBase="",$queryPath=""){
+	if(empty($urlBase) &&  empty($queryPath))
+		$urlQuery= $this->$urlBase.$this->queryPath;
+	else
+		$urlQuery = empty($queryPath) ? $this->checkUrlBase($urlBase).$this->$queryPath  : $this->$urlBase.$this->checkQueryPath($queryPath);
+	foreach ($query_vars as $key => $arrayValuealue)
+		$urlQuery= $urlQuery.$key.'='.$this->recorrerArray($arrayValuealue).'&'; 
+	return $urlQuery;
+}
+public function recorrerArray($arrayValue,$query=""){
+	foreach ($arrayValue as $item){ 
+		if(!is_array($item)){
+			($item==end($arrayValuealue))? $query=$query.$item : $query=$query.$item." AND ";
+		}
+		else{
+			return recorrerArray($item,$query)." OR ";
+		}
+	}
+	return $query;	
 }
 /** 
    * metodo encargado de agregar parametros para construir la url mediante una palabra clave y un valor. Si se desea agregar mas de un valor a la clave y no pisarlo $override debe ser true.
 **/
 public function add_query_vars($key,$value,$override=false){
-	if (!$override) 
+	if ($override) 
 		$query_vars[$key]=array($value);
 	else
-		(isset($query_vars[$key])) ? $query_vars[$key]= array_push($query_vars[$key],$value) : $query_vars[$key]=array($value);
+		(has_query_vars($key)) ? $query_vars[$key]= array_push($query_vars[$key],$value) : $query_vars[$key]=array($value);
 
 }
 public function remove_query_vars($key){
